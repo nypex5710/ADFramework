61b74bbf2dff16ba2427736ebd32de2a9a502a82
Alper Dursun <61089742+nypex5710@users.noreply.github.com>
2020-06-16 21:51:32 +0300

    add sef link function

--- src/ADFunctions.php
+++ src/ADFunctions.php
@@ -7,7 +7,7 @@ class ADFunctions
      * Safe post method
      * Post metodunu olası saldırılara karşı güvenli kılan fonksiyon
      */
-    function post($parameter, $condition = false)
+    public function post($parameter, $condition = false)
     {
         if($condition == false){
             $result = strip_tags(trim($_POST[$parameter]]));
@@ -21,7 +21,7 @@ class ADFunctions
      * Safe get method
      * Get metodunu olası saldırılara karşı güvenli kılan fonksiyon
      */
-    function get($parameter, $condition = false)
+    public function get($parameter, $condition = false)
     {
         if($condition == false){
             $result = strip_tags(trim($_GET[$parameter]));
@@ -35,7 +35,7 @@ class ADFunctions
      * Shows the user's ip address.
      * Kullanıcının ip adresini görmenizi sağlayar.
      */
-    function IP()
+    public function IP()
     {
         if(getenv("HTTP_CLIENT_IP")){
             $ip = getenv("HTTP_CLIENT_IP");
@@ -49,6 +49,21 @@ class ADFunctions
             $ip = getenv("REMOTE ADDR");
         }
         return $ip;
+    }
+
+    /**
+     * makes the data url friendly.
+     * Verilen girdiyi url dostu bir duruma getirir.
+     */
+    function sef_link($str)
+    {
+        $preg = array('Ç', 'Ş', 'Ğ', 'Ü', 'İ', 'Ö', 'ç', 'ş', 'ğ', 'ü', 'ö', 'ı', '+', '#', '.');
+        $match = array('c', 's', 'g', 'u', 'i', 'o', 'c', 's', 'g', 'u', 'o', 'i', 'plus', 'sharp', '');
+        $perma = strtolower(str_replace($preg, $match, $str));
+        $perma = preg_replace("@[^A-Za-z0-9\-_\.\+]@i", ' ', $perma);
+        $perma = trim(preg_replace('/\s+/', ' ', $perma));
+        $perma = str_replace(' ', '-', $perma);
+        return $perma;
     }
 }
 
